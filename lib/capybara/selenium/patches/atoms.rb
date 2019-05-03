243957a2a9eb83ebe94d127d6411d221f447fad9
Thomas Walpole <twalpole@gmail.com>
2019-05-03 09:09:08 -0700

    work on on the fly minimizing atom

--- lib/capybara/selenium/patches/atoms.rb
+++ lib/capybara/selenium/patches/atoms.rb
@@ -1,9 +1,17 @@
 # frozen_string_literal: true
+require 'uglifier'
 
 module CapybaraAtoms
   private
 
   def read_atom(function)
+    # @atoms ||= Hash.new { |hash, key|
+    #   hash[key] = ::Uglifier.compile(File.read(File.expand_path("../../atoms/#{key}.js", __FILE__)),
+    #     compress: {
+    #       negate_iife: false
+    #     }
+    #   )[0...-1]
+    # }
     @atoms ||= Hash.new { |hash, key|
       hash[key] = File.read(File.expand_path("../../atoms/#{key}.js", __FILE__))
     }
@@ -11,4 +19,4 @@ module CapybaraAtoms
   end
 end
 
-::Selenium::WebDriver::Remote::Bridge.prepend CapybaraAtoms
+# ::Selenium::WebDriver::Remote::Bridge.prepend CapybaraAtoms
